9b6082ad75bbea085997c400933c3a7e355f9077
enncy <877526278@qq.com>
2021-07-10 23:25:40 +0800

    fix path

--- index.ts
+++ index.ts
@@ -5,10 +5,10 @@ export * from './src/core/pioneer/Pioneer';
 export * from './src/core/script/InjectableScript';
 export * from './src/core/script/RunnableScript';
 export * from './src/core/script/Script';
-export * from './src/core/script/ScriptContext';
-export * from './src/core/script/ScriptEventPool';
-export * from './src/core/script/ScriptFactory';
-export * from './src/core/script/ScriptStorage';
+export * from './src/core/pioneer/ScriptContext';
+export * from './src/core/pioneer/ScriptEventPool';
+export * from './src/core/pioneer/ScriptFactory';
+export * from './src/core/pioneer/ScriptStorage';
 export * from './src/core/utils/WaitForScript';
 export * from './src/decorator/Inject'
 export * from './src/decorator/Injectable'
